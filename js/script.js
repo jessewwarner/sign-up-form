93fa04dbbfb5ab87911c4a37f4e26039ebdc2519
Jesse Warner <jesse.w.warner@gmail.com>
2023-09-26 19:02:10 -0400

    Add variables for input fields

---
+++ js/script.js
@@ -0,0 +1,8 @@
+firstName = document.querySelector('#first_name');
+lastName = document.querySelector('#last_name');
+email = document.querySelector('#mail');
+phone = document.querySelector('#phone');
+pwdInput = document.querySelector('#pwd');
+cpwdInput = document.querySelector('#cpwd');
+createAccountBtn = document.querySelector('.btn');
+signUpForm = document.querySelector('#sign-up-form');
